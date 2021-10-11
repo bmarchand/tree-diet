3ad59abc4a4f8857670c1aeca003f55b75232d1c
Bertrand Marchand <bertrand.marchand7@gmail.com>
2021-10-11 16:15:56 +0200

    versioned documentation

--- doc/conf.py
+++ doc/conf.py
@@ -35,11 +35,11 @@ extensions = ['sphinx.ext.autodoc',
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-html_sidebars = {
-    '**': [
-        'versioning.html',
-    ],
-}
+#html_sidebars = {
+#    '**': [
+#        'versioning.html',
+#    ],
+#}
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
